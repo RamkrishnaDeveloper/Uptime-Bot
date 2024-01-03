beaa6c0066cf87be26a12c0c7191ebe356fddc27
Ramkrishna <151381846+RamkrishnaDeveloper@users.noreply.github.com>
2024-01-03 18:40:35 +0530

    Use CareFully

---
+++ Assets/Config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  TOKEN:"",
+  MONGO_DB:"mongodb+srv://uptme:uptime@cluster0.actzyqm.mongodb.net/?retryWrites=true&w=majority",
+  CLIENT_ID:"1190510231948624022",
+  OWNERS: ["1128924613397721210"],
+  SUPPORT_SERVER: "https://discord.com/invite/Yc29SuFvDe",
+  CHANNELS: {
+    COMMANDS_LOGS: "1190514911781073007",
+    ERROR_COMMAND_LOGS: "1190514911781073007"
+  }
+}
---
+++ Assets/Config/emotes.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MESSAGE: {
+    y: "<a:yes_yes:1190511757819654265>", // yes 
+    x: "<a:wrong:1190511708465274891>", // no
+    i: "<:info:1190511666991988736>", // info
+    l: "<a:loading_bg:1190511620175188000>" // loading
+  }
+}
